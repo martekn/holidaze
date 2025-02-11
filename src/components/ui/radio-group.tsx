f41f6e7f9a5d3178ede3dfc9fefc43e59b52fbfa
martekn <marte.mk@hotmail.com>
2025-02-11 22:00:36 +0100

    Add shadcn radio group

---
+++ src/components/ui/radio-group.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import * as React from "react";
+import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
+
+import { cn } from "@/lib/utils";
+import { IconCircleFilled } from "@tabler/icons-react";
+
+const RadioGroup = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>
+>(({ className, ...props }, ref) => {
+  return <RadioGroupPrimitive.Root className={cn("grid gap-2", className)} {...props} ref={ref} />;
+});
+RadioGroup.displayName = RadioGroupPrimitive.Root.displayName;
+
+const RadioGroupItem = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
+>(({ className, ...props }, ref) => {
+  return (
+    <RadioGroupPrimitive.Item
+      ref={ref}
+      className={cn(
+        "block aspect-square h-24 w-24 rounded-full border border-input text-neutral-700 ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-ring",
+        className
+      )}
+      {...props}
+    >
+      <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
+        <IconCircleFilled className="h-[1.125rem] w-[1.125rem]" />
+      </RadioGroupPrimitive.Indicator>
+    </RadioGroupPrimitive.Item>
+  );
+});
+RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName;
+
+export { RadioGroup, RadioGroupItem };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -112,7 +112,8 @@ const config: Config = {
     borderRadius: {
       lg: "var(--radius)",
       md: "calc(var(--radius) - 2px)",
-      sm: "calc(var(--radius) - 4px)"
+      sm: "calc(var(--radius) - 4px)",
+      full: "50%"
     }
   },
   plugins: [tailwindcssAnimate]
