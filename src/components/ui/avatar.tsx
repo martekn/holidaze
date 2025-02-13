3b08839be7e2a8749b5f4a152a3fed84955fdb6f
martekn <marte.mk@hotmail.com>
2025-02-13 11:54:29 +0100

    Add avatar component

---
+++ src/components/ui/avatar.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import * as React from "react";
+import * as AvatarPrimitive from "@radix-ui/react-avatar";
+
+import { cn } from "@/lib/utils";
+import { IconUserFilled } from "@tabler/icons-react";
+
+interface AvatarProps extends React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root> {
+  alt?: string;
+  src?: string;
+}
+
+const Avatar = React.forwardRef<React.ElementRef<typeof AvatarPrimitive.Root>, AvatarProps>(
+  ({ alt, src, className, ...props }, ref) => (
+    <AvatarPrimitive.Root
+      ref={ref}
+      className={cn("relative flex h-40 w-40 shrink-0 overflow-hidden rounded-md", className)}
+      {...props}
+    >
+      <AvatarPrimitive.Image className={"aspect-square h-full w-full"} src={src} alt={alt} />
+      <AvatarPrimitive.Fallback
+        ref={ref}
+        className="flex h-full w-full items-center justify-center rounded-md bg-secondary-100"
+      >
+        <IconUserFilled className="h-1/2 w-1/2 text-secondary-300" />
+      </AvatarPrimitive.Fallback>
+      <div className="absolute inset-0 h-full w-full rounded-md border border-neutral-800/10"></div>
+    </AvatarPrimitive.Root>
+  )
+);
+Avatar.displayName = AvatarPrimitive.Root.displayName;
+
+export { Avatar };
