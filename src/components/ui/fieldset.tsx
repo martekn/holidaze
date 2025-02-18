860cb7a2a04a8bf9b703923f7e0bdb8a0402cf0f
martekn <marte.mk@hotmail.com>
2025-02-18 13:39:13 +0100

    Create fieldset component with legend

---
+++ src/components/ui/fieldset.tsx
@@ -0,0 +1,18 @@
+import { labelStyles } from "@/lib/styles";
+import { cn } from "@/lib/utils";
+import React from "react";
+
+interface FieldsetProps extends React.FieldsetHTMLAttributes<HTMLFieldSetElement> {
+  legend: string;
+}
+
+const Fieldset = ({ legend, children, className, ...props }: FieldsetProps) => {
+  return (
+    <fieldset className={cn("space-y-8", className)} {...props}>
+      <legend className={labelStyles()}>{legend}</legend>
+      {children}
+    </fieldset>
+  );
+};
+
+export { Fieldset };
