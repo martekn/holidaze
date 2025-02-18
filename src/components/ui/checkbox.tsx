aa33203bfdf2b04638629423af1adc2717ab1ab3
martekn <marte.mk@hotmail.com>
2025-02-18 13:50:39 +0100

    Reduce checkbox sizing

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -13,12 +13,12 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer block h-24 w-24 shrink-0 items-center justify-center rounded-md border border-input ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-ring data-[state=checked]:bg-neutral-700 data-[state=checked]:text-primary-foreground",
+      "peer block h-20 w-20 shrink-0 items-center justify-center rounded-md border border-input ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-ring data-[state=checked]:bg-neutral-700 data-[state=checked]:text-primary-foreground",
       className
     )}
     {...props}
   >
-    <CheckboxPrimitive.Indicator className={cn("flex items-center justify-center text-current")}>
+    <CheckboxPrimitive.Indicator className={cn("text-current flex items-center justify-center")}>
       <IconCheck className="h-16 w-16 stroke-2" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
