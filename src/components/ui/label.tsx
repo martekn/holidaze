bd7c898eb66cf97be16072a107a37d82d475c267
martekn <marte.mk@hotmail.com>
2025-02-18 13:02:22 +0100

    Move label styles out and create size variable

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -2,20 +2,17 @@
 
 import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
-import { cva, type VariantProps } from "class-variance-authority";
+import { LabelStylesProps, labelStyles } from "@/lib/styles";
 
 import { cn } from "@/lib/utils";
 
-const labelVariants = cva(
-  "text-base font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-);
+type LabelProps = React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & LabelStylesProps;
 
-const Label = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />
-));
+const Label = React.forwardRef<React.ElementRef<typeof LabelPrimitive.Root>, LabelProps>(
+  ({ className, size, ...props }, ref) => (
+    <LabelPrimitive.Root ref={ref} className={cn(labelStyles({ size }), className)} {...props} />
+  )
+);
 Label.displayName = LabelPrimitive.Root.displayName;
 
 export { Label };
