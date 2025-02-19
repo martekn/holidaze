35f690753e5b39f7da4802e4cbb0151a6b63ceef
martekn <marte.mk@hotmail.com>
2025-02-19 17:41:38 +0100

    Update input and label styling according to design

--- src/lib/styles/input-styles.ts
+++ src/lib/styles/input-styles.ts
@@ -1,7 +1,7 @@
 import { cva, VariantProps } from "class-variance-authority";
 
 export const inputStyles = cva(
-  "peer flex w-full items-center rounded-lg border bg-background py-12 text-base ring-offset-background transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground focus-visible:border-neutral-700 focus-visible:outline-none focus-visible:ring-1 gap-8 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 placeholder:text-muted-foreground",
+  "peer flex w-full items-center rounded-lg border bg-background py-12 text-base ring-offset-background transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground focus-visible:border-neutral-700 focus-visible:outline-none focus-visible:ring-1 gap-8 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 placeholder:text-muted-foreground font-medium placeholder:font-normal",
   {
     variants: {
       layout: {
@@ -23,6 +23,6 @@ export const inputStyles = cva(
 export const inputWrapperStyles = "relative";
 
 export const inputIconStyles =
-  "h-20 w-20 absolute left-12 top-1/2 -translate-y-1/2 text-muted-foreground";
+  "h-20 w-20 absolute left-12 top-1/2 -translate-y-1/2 text-muted-foreground stroke-[1.5]";
 
 export type InputStylesProps = VariantProps<typeof inputStyles>;
--- src/lib/styles/label-styles.ts
+++ src/lib/styles/label-styles.ts
@@ -1,7 +1,7 @@
 import { cva, VariantProps } from "class-variance-authority";
 
 export const labelStyles = cva(
-  "text-base leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+  "text-base text-foreground leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
   {
     variants: {
       size: {
