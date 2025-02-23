434ea24ab6f910a46ef4da5ac46dcf5bb27d6ef0
martekn <marte.mk@hotmail.com>
2025-02-23 15:04:15 +0100

    Cleanup styles index file for non existing files

--- src/lib/styles/index.ts
+++ src/lib/styles/index.ts
@@ -1,6 +1,5 @@
 export * from "./button-styles";
 export * from "./input-styles";
 export * from "./label-styles";
-export * from "./input-group-styles";
 export * from "./floating-label-styles";
 export * from "./form-item-styles";
