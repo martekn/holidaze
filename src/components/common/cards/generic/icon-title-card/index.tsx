cc8ebee1f89440a0ffb48f6509e64a83b41bf8ff
martekn <marte.mk@hotmail.com>
2025-03-04 15:34:45 +0100

    Allow classnames passed into card

--- src/components/common/cards/generic/icon-title-card/index.tsx
+++ src/components/common/cards/generic/icon-title-card/index.tsx
@@ -7,17 +7,19 @@ type IconTitleCardProps = {
   title: string;
   icon: TablerIcon;
   children: React.ReactNode;
+  className?: string;
   size?: "sm" | "lg";
 };
 const IconTitleCard = ({
   title,
   icon: Icon,
   children,
+  className,
   size = "lg",
   ...props
 }: IconTitleCardProps) => {
   return (
-    <Card variant={"outline"} padding={"lg"} className="space-y-8" {...props}>
+    <Card variant={"outline"} padding={"lg"} className={cn("space-y-8", className)} {...props}>
       <div className="flex items-center justify-between gap-16">
         <CardTitle variant={size === "lg" ? "heading5" : "heading6"}>{title}</CardTitle>{" "}
         <Icon className={cn("stroke-[1.5]", size === "lg" ? "w-3 h-32" : "h-20 w-20")} />
