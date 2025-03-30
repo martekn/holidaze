ad38f51553a1ecb36b0ff965b5ee8bb803444772
martekn <marte.mk@hotmail.com>
2025-03-30 17:05:01 +0200

    Create form layout

---
+++ src/components/layout/form-layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Logo } from "../ui/logo";
+import Container from "../ui/container";
+
+const FormLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen space-y-64">
+      <Container variant={"fullBleed"}>
+        <header className="pt-16">
+          <Logo className="w-96" />
+        </header>
+      </Container>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default FormLayout;
