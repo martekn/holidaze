f7b445c6ccd652d914ee1e614d08dd6da321575f
martekn <marte.mk@hotmail.com>
2025-04-01 15:05:56 +0200

    Setup profile based constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,12 @@
+import { IconBook, IconHome, IconSettings } from "@tabler/icons-react";
+
 export const API_BASE_URL = "https://v2.api.noroff.dev";
+
+export const PROFILE_LINKS = [
+  { text: "Bookings", href: "/profile/bookings", icon: IconBook },
+  { text: "Listings", href: "/profile/hosted-listings", icon: IconHome },
+  { text: "Settings", href: "/profile/settings", icon: IconSettings }
+];
+
+export const API_AVATAR_PLACEHOLDER =
+  "https://images.unsplash.com/photo-1579547945413-497e1b99dac0?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&q=80&h=400&w=400";
