a0a60993f3396b612149b927d0de9d6e83eccb57
martekn <marte.mk@hotmail.com>
2025-04-05 10:42:28 +0200

    Setup fallbacks for listing api response

--- src/components/common/cards/listing/hosted-listing-card/index.tsx
+++ src/components/common/cards/listing/hosted-listing-card/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Card, CardImage, CardTitle } from "@/components/ui/card";
 import { TBaseListing } from "@/lib/schema";
 import { getFormattedDate } from "@/lib/utils/get-formatted-date";
+import { LISTING_NAME_PLACEHOLDER } from "@/lib/constants";
 
 type HostedListingCardProps = {
   listing: TBaseListing;
@@ -15,11 +16,11 @@ const HostedListingCard = ({ listing, ...props }: HostedListingCardProps) => {
 
   return (
     <Card className="relative cursor-pointer space-y-12" {...props}>
-      <CardImage ratio={5 / 3} src={media[0].url} alt={media[0].alt} scaleOnHover />
+      <CardImage ratio={5 / 3} src={media[0]?.url} alt={media[0]?.alt} scaleOnHover />
       <div className="space-y-4">
         <span className="text-sm text-muted-foreground">Last updated {formattedDate}</span>
         <CardTitle asChild ellipse fullCardLink hoverEffect>
-          <Link href={`/profile/hosted-listings/${id}`}>{name}</Link>
+          <Link href={`/profile/hosted-listings/${id}`}>{name || LISTING_NAME_PLACEHOLDER}</Link>
         </CardTitle>
       </div>
     </Card>
