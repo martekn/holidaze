538b2070d929ce96d3d2e6c1c685d4b5b8c85475
martekn <marte.mk@hotmail.com>
2025-04-18 21:19:26 +0200

    Add scrolling overlay to details page

--- src/app/(public)/listings/[id]/components/booking-form.tsx
+++ src/app/(public)/listings/[id]/components/booking-form.tsx
@@ -20,7 +20,7 @@ import { calculateBookingDetails } from "@/lib/utils/calculate-booking-details";
 import { getBookedDates } from "@/lib/utils/get-booked-dates";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { DateRange } from "react-day-picker";
 import { useForm } from "react-hook-form";
 
@@ -60,64 +60,103 @@ const BookingForm = ({
 
   const dateRange = form.watch("dateRange");
   const { nights, totalPrice, isValidRange } = calculateBookingDetails(dateRange, listing.price);
+  const formRef = useRef<HTMLDivElement>(null);
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
+  useEffect(() => {
+    const ref = formRef.current;
+    if (!ref) return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsFormVisible(entry.isIntersecting);
+      },
+      { root: null, rootMargin: "0px", threshold: 0.1 }
+    );
+
+    observer.observe(ref);
+
+    return () => {
+      if (ref) {
+        observer.unobserve(ref);
+      }
+    };
+  }, []);
+
+  const onButtonScroll = () => {
+    if (formRef.current)
+      formRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+        inline: "center"
+      });
+  };
 
   return (
-    <Card variant={"outline"} padding={"lg"} className="space-y-24">
-      <Price price={listing.price} variant={"lg"} />
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-24">
-          <FormField
-            control={form.control}
-            name="dateRange"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <DateRangePicker
+    <>
+      {!isFormVisible && (
+        <div className="fixed inset-x-0 bottom-0 z-10 flex w-full items-center justify-between border-t bg-neutral-100 p-16 shadow-md">
+          <Price price={listing.price} variant={"lg"} />
+          <Button onClick={onButtonScroll}>Book</Button>
+        </div>
+      )}
+      <Card variant={"outline"} padding={"lg"} className="space-y-24" ref={formRef}>
+        <Price price={listing.price} variant={"lg"} />
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-24">
+            <FormField
+              control={form.control}
+              name="dateRange"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <DateRangePicker
+                      borderStyle={"light"}
+                      label="Check in - Check out"
+                      value={field.value}
+                      placeholder="Select dates"
+                      onChange={(date: DateRange | undefined) => {
+                        field.onChange(date ?? { from: undefined, to: undefined });
+                      }}
+                      bookedDates={bookedDates}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="guestCount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Guests</FormLabel>
+                  <GuestCountInput
+                    {...field}
+                    id="test"
                     borderStyle={"light"}
-                    label="Check in - Check out"
-                    value={field.value}
-                    placeholder="Select dates"
-                    onChange={(date: DateRange | undefined) => {
-                      field.onChange(date ?? { from: undefined, to: undefined });
-                    }}
-                    bookedDates={bookedDates}
+                    maxGuests={listing.maxGuests}
                   />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="guestCount"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Guests</FormLabel>
-                <GuestCountInput
-                  {...field}
-                  id="test"
-                  borderStyle={"light"}
-                  maxGuests={listing.maxGuests}
-                />
-              </FormItem>
-            )}
-          />
+                </FormItem>
+              )}
+            />
 
-          {isValidRange && (
-            <div className="flex items-center justify-between border-t pt-24">
-              <span>
-                Total price for {nights} night{nights > 1 ? "s" : null}
-              </span>
-              <span className={headingStyles({ variant: "heading5" })}>${totalPrice}</span>
-            </div>
-          )}
+            {isValidRange && (
+              <div className="flex items-center justify-between border-t pt-24">
+                <span>
+                  Total price for {nights} night{nights > 1 ? "s" : null}
+                </span>
+                <span className={headingStyles({ variant: "heading5" })}>${totalPrice}</span>
+              </div>
+            )}
 
-          <Button type="submit" className="w-full">
-            Continue to booking
-          </Button>
-        </form>
-      </Form>
-    </Card>
+            <Button type="submit" className="w-full">
+              Continue to booking
+            </Button>
+          </form>
+        </Form>
+      </Card>
+    </>
   );
 };
 
